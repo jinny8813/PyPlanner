d0b779eb5f916a2c5a604aa1750e1fae0b2de0c5
chingyi lan <jinny8813@gmail.com>
2024-08-31 10:26:05 +0800

    set the attribute of font color and start day

---
+++ src/attributes/background_color.py
@@ -0,0 +1,20 @@
+from pptx.dml.color import RGBColor
+
+def get_bg_color_info(bg_color):
+    select_bg_color = {
+        "light": {
+            "font_color_section": RGBColor(51, 51, 51),
+            "font_color_page_title": RGBColor(92, 92, 92),
+            "font_color_C": RGBColor(133, 133, 133),
+            "nav_bg_color": RGBColor(214, 214, 214),
+            "nav_bg_font_color": RGBColor(92, 92, 92)
+        },
+        "dark": {
+            "font_color_section": RGBColor(51, 51, 51),
+            "font_color_page_title": RGBColor(235, 235, 235),
+            "font_color_C": RGBColor(194, 194, 194),
+            "nav_bg_color": RGBColor(214, 214, 214),
+            "nav_bg_font_color": RGBColor(92, 92, 92)
+            }
+        }
+    return select_bg_color.get(bg_color, select_bg_color["light"])
--- src/main.py
+++ src/main.py
@@ -1,6 +1,19 @@
 import os
 from pptx import Presentation
 
+from features.chapter_page import create_calendar_slides
+from features.main_calendar import create_main_calendar_slides
+
+my_index_list = []
+ppt_index_count = 0
+
 os.chdir(r'src')
 prs = Presentation('assets/layout-2425-light-chinese.pptx')
+
+ppt_index_count = create_calendar_slides(prs, my_index_list, ppt_index_count, ["Journal","Calendar"], ["Monthly | Calendar","月行事曆"])
+ppt_index_count = create_main_calendar_slides(prs, ppt_index_count)
+
+print(my_index_list)
+print(ppt_index_count)
+
 prs.save(f'prep.pptx')
