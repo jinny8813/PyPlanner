621f529ebc39bd96b9345e64882d1d596bf04946
chingyi lan <jinny8813@gmail.com>
2024-09-02 08:07:34 +0800

    create monthly tracker and add weekday

--- src/units/set_elements.py
+++ src/units/set_elements.py
@@ -1,5 +1,6 @@
 from pptx.enum.text import PP_ALIGN
 from pptx.util import Pt
+import calendar
 
 from set_attribute import orig_font_color_element
 
@@ -24,3 +25,24 @@ def set_lunar_element(slide,lunar_text,left, top, width, height):
     p.font.name = "Arial"
     p.font.color.rgb = orig_font_color_element
     p.alignment = PP_ALIGN.CENTER
+
+def set_small_calendar(slide,month_first_day,left, top,week_list):   
+    o_left = left
+    width = height = Pt(20)
+    for e in week_list:
+        set_date_element(slide,e,left, top, width, height)
+        left += width
+    top += height
+    for j in range(calendar.monthrange(month_first_day.year,month_first_day.month)[1]):
+        if j == 0:
+            if week_list[0] == "M":
+                left = o_left + width * month_first_day.weekday()
+            else:
+                left = o_left + width * (month_first_day.weekday()+1)
+        if left > o_left + width *6:
+            top += height
+            left = o_left 
+            if j == 0:
+                top -= height
+        set_date_element(slide,str(j+1),left, top, width, height)
+        left += width
