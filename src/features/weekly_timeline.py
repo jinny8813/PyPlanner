7968777a4cb936e87ae1a41ca5c3b0e164684066
chingyi lan <jinny8813@gmail.com>
2024-09-03 09:09:17 +0800

    small change of yearly count

--- src/features/weekly_timeline.py
+++ src/features/weekly_timeline.py
@@ -5,19 +5,19 @@ import calendar
 
 from units.set_titles import set_page_title
 from units.set_elements import set_date_element,set_lunar_element
-from set_attribute import orig_start_week,orig_start_date
+from set_attribute import orig_start_week,orig_start_date,orig_start_month
 from set_attribute import orig_lunar_calender_count
 from attributes.language import lunar_content
 
 def create_timeline_slides(prs, ppt_count):
     today = orig_start_date
-    this_year = int(today.strftime("%Y"))
+    this_year = int(orig_start_month.strftime("%Y"))
     lunar_count = orig_lunar_calender_count
     week_count = orig_start_week
     while today < orig_start_date+relativedelta(years=1):
         layout = prs.slide_layouts[9]
         slide = prs.slides.add_slide(layout)
-        if week_count == 1 and orig_start_week !=1:
+        if week_count == 1 and today != orig_start_date:
             this_year += 1        
         set_page_title(slide,"{} Week {} Timeline".format(this_year,week_count))
         top = Pt(83)
@@ -34,5 +34,7 @@ def create_timeline_slides(prs, ppt_count):
             today += timedelta(days=1)
             left += width
         week_count +=1
+        if week_count>52:
+            week_count = 1
     ppt_count += 53
     return ppt_count
