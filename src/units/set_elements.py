9656db7f7ac5b976589cd43be5d6b3d6d1c56294
chingyi lan <jinny8813@gmail.com>
2024-09-03 09:02:18 +0800

    create weekly grid

--- src/units/set_elements.py
+++ src/units/set_elements.py
@@ -4,6 +4,15 @@ import calendar
 
 from set_attribute import orig_font_color_element
 
+def set_date_element_grid(slide,date_value,left, top, width, height):
+    tb = slide.shapes.add_textbox(left, top, width, height)
+    p = tb.text_frame.paragraphs[0]
+    p.text = date_value
+    p.font.size = Pt(24)
+    p.font.name = "Arial"
+    p.font.color.rgb = orig_font_color_element
+    p.alignment = PP_ALIGN.CENTER
+
 def set_date_element(slide,date_value,left, top, width, height, lunar_text=None):
     tb = slide.shapes.add_textbox(left, top, width, height)
     p = tb.text_frame.paragraphs[0]
